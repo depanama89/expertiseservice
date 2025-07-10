dcc0aff1f08357462a329ce2755ff96bad33b1a1
depanama <kudiatutomanitu@gmail.com>
2025-07-10 14:23:14 +0100

    added h in header component

--- frontend/src/composants/header/Header.tsx
+++ frontend/src/composants/header/Header.tsx
@@ -4,7 +4,7 @@ import bgheader from "../../assets/bg-header.png";
 import Hero from "../hero/Hero";
 const Header = () => {
   return (
-    <div className="w-full h-[450px] md:h-[550px] lg:h-[600px] bg-cover " style={{ backgroundImage: `url(${bgheader})`, backgroundPosition: "center", backgroundSize: "cover" }}>
+    <div className="w-full h-[550px] md:h-[550px] lg:h-[600px] bg-cover " style={{ backgroundImage: `url(${bgheader})`, backgroundPosition: "center", backgroundSize: "cover" }}>
       <NavBar />
       <Hero />
     </div>
